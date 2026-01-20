739c31f787eb665b2a2b55fc51287ccd6672576e
Timothy J. McMichael <timmcmic@microsoft.com>
2026-01-20 07:09:37 -0500

    Updating

--- get-MultipleXMLFiles.ps1
+++ get-MultipleXMLFiles.ps1
@@ -21,4 +21,29 @@ function get-MultipleXMLFiles
     $files = get-ChildItem -path $rootPath -name $fileName -Recurse
 
     out-logfile -string ("File Count: "+$files.count.tostring())
+
+    $returnList = [System.Collections.Generic.List[psCustomObject]]::new()
+
+    foreach ($file in $files)
+    {
+        out-logfile -string ("Processing file: "+$file.FullName)
+
+        $data = Import-Clixml -Path $file.FullName
+
+        out-logfile -string ("Processing entry count: "+$data.Count.tostring())
+
+        $returnList.add($data)
+
+        out-logfile -string ("Return list count: "+$returnList.Count.tostring())
+    }
+
+    out-logfile -string ("Return list imported count: "+$returnList.Count.tostring())
+
+    $returnList = $returnList | Sort-Object -Property ID,Address -Unique
+
+    out-logfile -string ("Sorted return list imported count: "+$returnList.Count.tostring())
+
+    out-logfile -string "End Get-MultipleXMLFiles"
+
+    return $returnList
 }
