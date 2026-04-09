792959223b9fac99951aeb7dc6ea6545990bff22
Tim McMichael <timmcmic@microsoft.com>
2026-04-08 22:35:59 -0400

    Updating

--- receive-CompletedJobs.ps1
+++ receive-CompletedJobs.ps1
@@ -2,24 +2,7 @@ function receive-completedJobs
 {
     out-logfile -string "Start Receive-CompletedJobs"
 
-    try {
-        $jobs = get-Job -state Completed
-    }
-    catch {
-        out-logfile -string $_
-        out-logfile -string "Unable to retrieve jobs."
-    }
-
-    foreach ($job in $jobs)
-    {
-        try {
-            Receive-Job -Id $job.Id -errorAction STOP
-        }
-        catch {
-            out-logfile -string $_
-            out-logfile -string "Unable to receive job."
-        }
-    }
+    get-Job -state Completed | Receive-Job
         
     out-logfile -string "End Receive-CompletedJobs"
 }
